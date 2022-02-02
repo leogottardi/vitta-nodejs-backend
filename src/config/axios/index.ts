4cf22a9bdd00c5bead9e7b6edf4f690028ce5c56
Leonardo Gottardi <leomgottardi@gmail.com>
2022-02-02 07:44:56 -0300

    feat: adicionando envs na configuraçao do axios

--- src/config/axios/index.ts
+++ src/config/axios/index.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-axios.defaults.headers.post['Authorization'] = 'Basic Y3VzdG9tZXJzOjQ1MzAwMGY3LTQ3YTAtNDQ4OS1iYzQ3LTg5MWM3NDI2NTBlMg==';
+axios.defaults.headers.post['Authorization'] = `Basic ${process.env.BASIC_AUTH}`;
 
 const vitta_sso = axios.create({
-  baseURL: "https://accounts.seguros.vitta.com.br/",
+  baseURL: `${process.env.VITTA_SSO_BASE_URL}`,
 });
 
 export { vitta_sso };
